f9a5d45a5858377de2efa181e39cfc4091b1755a
Nate Klaiber <nate@theklaibers.com>
2014-06-04 09:53:35 -0400

    Quick notes on the Configuration object

--- lib/hue/client/configuration.rb
+++ lib/hue/client/configuration.rb
@@ -3,6 +3,10 @@ module Hue
     class Configuration
       attr_accessor :ip_address, :username
 
+      # Allows you to create a Configuration object to
+      # hold the necessary keys and values.
+      #
+      # @return [Hue::Client::Configuration] Self
       def initialize(options={})
         if block_given?
           yield(self)
